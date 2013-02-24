9c02aa66ac751a88ea7ecc6f27aeda68d9c3bcaa
derekfogge <dfogge@me.com>
2013-02-24 17:38:00 -0500

    clean up javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-var day=new Date();
-
-var weekday=new Array(7);
-weekday[0]="Sunday";
-weekday[1]="Monday";
-weekday[2]="Tuesday";
-weekday[3]="Wednesday";
-weekday[4]="Thursday";
-weekday[5]="Friday";
-weekday[6]="Saturday";
-=======
 var day = new Date();
 
 var weekday = new Array(7);
@@ -20,7 +8,6 @@ var weekday = new Array(7);
 		weekday[4] = 'Thursday';
 		weekday[5] = 'Friday';
 		weekday[6] = 'Saturday';
->>>>>>> gh-pages
 
 var today = weekday[day.getDay()];
 
