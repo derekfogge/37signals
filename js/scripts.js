4ce6cef5c74d42b24d32f48e2211d4dff0c287d4
derekfogge <dfogge@me.com>
2013-04-12 01:58:40 -0400

    add jquery product hovers

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,64 @@ var weekday = new Array(7);
 var today = weekday[day.getDay()];
 
 document.getElementById('today').innerHTML=today;
+
+
+
+$(document).ready(function () {
+
+	var mouseIsOverHeadline= false;
+	var productHeadline = null;
+	var product = '.product';
+	var mainHeadline = '#headline';
+
+//Sets the true/false var when mouse is over headline
+	$(headline).mouseover(function(){
+	   mouseIsOverHeadline = true;
+	}).mouseleave(function () {
+	   mouseIsOverHeadline = false;
+	});
+
+
+//When ever you mouse over a product div this gets executed to hide the main headline and show the desired product headline
+
+	$(product).mouseover(function () {
+		$(mainHeadline).stop().hide();
+		$(productHeadline).stop().hide();
+
+		productHeadline = '#headline-' + $(this).attr('id');
+
+		$(productHeadline).show();
+		console.log('mouseover product');
+	});
+
+	$('.product-headline').mouseover(function () {
+		$(mainHeadline).stop().hide();
+		$(productHeadline).stop().show();
+		console.log('mouseover headline');
+	});
+
+	$(product).mouseleave(function (){
+		$(productHeadline).stop().delay(1000).hide(0);
+		$(mainHeadline).delay(1000).show(0);
+		console.log('mouseleave product');
+	});
+
+	$('.product-headline').mouseleave(function (){
+		$(productHeadline).stop().delay(1000).hide(0);
+		$(mainHeadline).delay(1000).show(0);
+		console.log('mouseleave headline');
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
